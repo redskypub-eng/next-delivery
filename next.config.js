db03fbd7157a879941a9f51c45471d152ba81648
redskypub-eng <redskypub@gmail.com>
2026-04-30 13:38:14 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'], 
-    })
-    return config;
-  }
-}
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
